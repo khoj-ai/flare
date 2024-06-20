7d56ad76228d24f0f54384932b01a5e64aefb057
vr-varad <114755221+vr-varad@users.noreply.github.com>
2024-06-20 10:45:53 +0530

    Resolve Errors (#4)
    * Added Dark and Light mode and text animation
    
    * Adjusting Mobile Layout
    
    * Additional minor updates for styling, copy
    
    ---------
    
    Co-authored-by: vr-varad <varadgupta21#gmail.com>
    Co-authored-by: sabaimran <narmiabas@gmail.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+import { Container, Engine } from "tsparticles-engine";
+
+declare global {
+    interface Window {
+        particlesInit: (engine: Engine) => Promise<void>;
+        particlesLoaded: (container: Container) => void;
+    }
+}
+
+export {};
